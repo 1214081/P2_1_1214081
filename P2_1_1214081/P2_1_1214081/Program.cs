80902017c7bba7d9071b19f4a403fd352fc67221
1214081 <114393418+1214081@users.noreply.github.com>
2022-09-26 16:38:31 +0700

    ini tugas

---
+++ P2_1_1214081/P2_1_1214081/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1214081
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Silahkan Masukkan Angka Pertama dan Angka Kedua");
+            int angkaPertama = int.Parse(Console.ReadLine());
+            int angkaKedua = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Angka Pertama    = "+angkaPertama);
+            Console.WriteLine("Angka Kedua      = " +angkaKedua);
+
+            Console.WriteLine("");
+
+            Console.Write(angkaPertama );
+            Console.Write(" + ");
+            Console.Write(angkaKedua+" = ");
+            Console.WriteLine(angkaPertama+angkaKedua);
+
+            Console.Write(angkaPertama);
+            Console.Write(" - ");
+            Console.Write(angkaKedua + " = ");
+            Console.WriteLine(angkaPertama - angkaKedua);
+
+            Console.Write(angkaPertama);
+            Console.Write(" * ");
+            Console.Write(angkaKedua + " = ");
+            Console.WriteLine(angkaPertama * angkaKedua);
+
+            Console.Write(angkaPertama);
+            Console.Write(" / ");
+            Console.Write(angkaKedua + " = ");
+            Console.WriteLine(angkaPertama / angkaKedua);
+        }
+    }
+}
